c73bcb24ac88b797cebb0d9ab06e632b9ef16ccb
Reece Pritchard <rapritchard96@gmail.com>
2019-11-06 21:14:28 +0000

    uibuild

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('build'));
 
 morgan.token('data', function (req, res) { 
     return JSON.stringify(req.body)
