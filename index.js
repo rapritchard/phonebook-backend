d3ff8a3b860454924b81aabe98e34310c2beea93
Reece Pritchard <rapritchard96@gmail.com>
2019-11-09 00:11:52 +0000

    Converted all HTTP requests to work with mongoDB and added error handling.

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const cors = require('cors');
 const app = express();
 const Phonebook = require('./models/phonebook');
 
-app.use(cors());
-app.use(bodyParser.json());
 app.use(express.static('build'));
+app.use(bodyParser.json());
+app.use(cors());
 
 morgan.token('data', function (req, res) { 
     return JSON.stringify(req.body)
@@ -26,13 +26,19 @@ app.get('/api/persons', (req, res) => {
     });
 });
 
-app.get('/api/persons/:id', (req, res) => {
-    Phonebook.findById(req.params.id).then(person => {
-        res.json(person.toJSON());
-    });
+app.get('/api/persons/:id', (req, res, next) => {
+    Phonebook.findById(req.params.id)
+        .then(person => {
+            if(person) {
+                res.json(person.toJSON());
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(error => next(error));
 });
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body;
 
     if(!body.name) {
@@ -52,22 +58,43 @@ app.post('/api/persons', (req, res) => {
         number: body.number,
     });
 
-    person.save().then(savedPerson => {
-        res.json(savedPerson.toJSON());
-    });
+    person.save()
+        .then(savedPerson => {
+            res.json(savedPerson.toJSON());
+        })
+        .catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter(person => person.id !== id);
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body;
 
-    res.status(204).end();
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Phonebook.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson.toJSON());
+        })
+        .catch(error => next(error));
+});
+
+app.delete('/api/persons/:id', (req, res, next) => {
+    const id = Number(req.params.id);
+    Phonebook.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
 app.get('/info', (req, res) => {
-    const total = persons.length;
-    const date = new Date();
-    res.send(`<p>Phonebook has info for ${total} people</p><p>${date}</p>`)
+    Phonebook.find({}).then(phonebook => {
+        const total = phonebook.length;
+        const date = new Date();
+        res.send(`<p>Phonebook has info for ${total} people</p><p>${date}</p>`);
+    })
 });
 
 const unknownEndpoint = (req, res) => {
@@ -75,8 +102,20 @@ const unknownEndpoint = (req, res) => {
         error: 'unknown endpoint'
     });
 };
-
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+      return res.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+// handler of requests with result to errors
+app.use(errorHandler)
 
 const PORT = `${process.env.PORT}` || 3001;
 app.listen(PORT, () => {
