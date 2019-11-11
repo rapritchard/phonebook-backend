a0e400f61964cf08f96c69c95836a7de88bf23aa
Reece Pritchard <rapritchard96@gmail.com>
2019-11-11 21:31:57 +0000

    Added eslint to project

--- index.js
+++ index.js
@@ -11,116 +11,113 @@ app.use(express.static('build'));
 app.use(bodyParser.json());
 app.use(cors());
 
-morgan.token('data', function (req, res) { 
-    return JSON.stringify(req.body)
-});
+morgan.token('data', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
 app.get('/', (req, res) => {
-    res.send('<h1>Phonebook</h1>');
+  res.send('<h1>Phonebook</h1>');
 });
 
 app.get('/api/persons', (req, res) => {
-    Phonebook.find({}).then(people => {
-        res.json(people.map(people => people.toJSON()));
-    });
+  Phonebook.find({}).then((people) => {
+    res.json(people.map((person) => person.toJSON()));
+  });
 });
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Phonebook.findById(req.params.id)
-        .then(person => {
-            if(person) {
-                res.json(person.toJSON());
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch(error => next(error));
+  Phonebook.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person.toJSON());
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body;
+  const { body } = req;
 
-    if(!body.name) {
-        return res.status(400).json({
-            error: 'name missing'
-        });
-    }
-
-    if(!body.number) {
-        return res.status(400).json({
-            error: 'number missing'
-        });
-    }
-
-    const person = new Phonebook({
-        name: body.name,
-        number: body.number,
+  if (!body.name) {
+    return res.status(400).json({
+      error: 'name missing',
     });
+  }
 
-    person.save()
-        .then(savedPerson => savedPerson.toJSON())
-        .then(savedAndFormattedPerson => {
-            res.json(savedAndFormattedPerson);
-        })
-        .catch(error => next(error));
+  if (!body.number) {
+    return res.status(400).json({
+      error: 'number missing',
+    });
+  }
+
+  const person = new Phonebook({
+    name: body.name,
+    number: body.number,
+  });
+
+  person.save()
+    .then((savedPerson) => savedPerson.toJSON())
+    .then((savedAndFormattedPerson) => {
+      res.json(savedAndFormattedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body;
+  const { body } = req;
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
 
-    Phonebook.findByIdAndUpdate(req.params.id, person, { new: true })
-        .then(updatedPerson => {
-            res.json(updatedPerson.toJSON());
-        })
-        .catch(error => next(error));
+  Phonebook.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson.toJSON());
+    })
+    .catch((error) => next(error));
 });
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = Number(req.params.id);
-    Phonebook.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end();
-        })
-        .catch(error => next(error));
+  Phonebook.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.get('/info', (req, res) => {
-    Phonebook.find({}).then(phonebook => {
-        const total = phonebook.length;
-        const date = new Date();
-        res.send(`<p>Phonebook has info for ${total} people</p><p>${date}</p>`);
-    })
+  Phonebook.find({}).then((phonebook) => {
+    const total = phonebook.length;
+    const date = new Date();
+    res.send(`<p>Phonebook has info for ${total} people</p><p>${date}</p>`);
+  });
 });
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({
-        error: 'unknown endpoint'
-    });
+  res.status(404).send({
+    error: 'unknown endpoint',
+  });
 };
 // handler of requests with unknown endpoint
 app.use(unknownEndpoint);
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError' && error.kind === 'ObjectId') {
-      return res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message });
-    }
-  
-    next(error)
-}
+  console.error(error.message);
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' });
+  } if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
 // handler of requests with result to errors
-app.use(errorHandler)
+app.use(errorHandler);
 
 const PORT = `${process.env.PORT}` || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
--- models/phonebook.js
+++ models/phonebook.js
@@ -8,26 +8,28 @@ const url = `${process.env.MONGODB_URI}`;
 console.log('connecting to ', url);
 
 mongoose.connect(url, { useNewUrlParser: true })
-    .then(result => {
-        console.log('connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message);
-    });
+  .then((result) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const phonebookSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true, minlength: 3},
-    number: {type: String, required: true, minlength: 8},
+  name: {
+    type: String, required: true, unique: true, minlength: 3,
+  },
+  number: { type: String, required: true, minlength: 8 },
 });
 
 phonebookSchema.plugin(uniqueValidator);
 
 phonebookSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v; 
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
 module.exports = mongoose.model('Person', phonebookSchema);
